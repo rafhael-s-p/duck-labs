e42843211d9ff991cbc02048ed8ad4ee529e4027
Rafhael <rafhael.dev@gmail.com>
2024-09-18 15:43:08 -0300

    feat: measurements of weight and length

--- resources/main.js
+++ resources/main.js
@@ -23,3 +23,52 @@ let experiment2 = {
     endTime: new Date(2024, 9, 18, 21, 33),
     complete: true
 };
+
+// Create Measurement Objects
+let measurement1 = {
+    id: 1,
+    unit: "kg",
+    value: 42,
+    time: 'PT2M12S' // The time value represents a period of time elapsed since the start of the experiment and should be expressed as a string value in iso format
+};
+let measurement2 = {
+    id: 2,
+    unit: "kg",
+    value: 40,
+    time: 'PT3M10S'
+};
+let measurement3 = {
+    id: 3,
+    unit: "kg",
+    value: 3,
+    time: 'PT3M55S'
+};
+let measurement4 = {
+    id: 4,
+    unit: "m",
+    value: 12,
+    time: 'PT20M'
+};
+let measurement5 = {
+    id: 5,
+    unit: "m",
+    value: 10,
+    time: 'PT1H22M10S'
+};
+// Create and populate and array of Measurement objects
+let measurements = [measurement1, measurement2, measurement3];
+
+/*--- Function Section ---*/
+// Convert between pounds and kilograms
+function lb2kg(lb) {
+    return lb / LB_PER_KG;
+}
+function kg2lb(kg) {
+    return kg * LB_PER_KG;
+}
+
+/*--- Test Section ---*/
+// Convert between pounds and kilograms
+let lb = kg2lb(measurement1.value);
+let kg = lb2kg(lb);
+console.log("Pounds: " + lb + " Kilograms: " + kg);
